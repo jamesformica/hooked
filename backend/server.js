0db6d7c3b8c618bb7e53da521e152eba7a755be9
Aaron Vanston <aaron.vanston@rea-group.com>
2019-05-29 10:53:39 +1000

    refactor: server to use new syntax

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
-const WebSocket = require('ws');
+const WebSocket = require('ws')
 
-const wss = new WebSocket.Server({ port: 1019 });
+const PORT = 1019
+const wss = new WebSocket.Server({ port: PORT })
+
 console.log("starting server on https://localhost:1019")
 
-wss.on('connection', function connection(ws, req) {
-  const ip = req.connection.remoteAddress;
+wss.on('connection', (ws, req) => {
+  const ip = req.connection.remoteAddress
 
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+  ws.on('message', message => {
+    console.log('received: %s', message)
   });
 
-  console.log(`Client connected from ${ip}`)
+  console.log(`client connected from ${ip}`)
 
-  ws.send('something');
+  ws.send('something')
 });
