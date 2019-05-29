0cff04b694e711b2262961c1de82be370973ba65
Aaron Vanston <aaron.vanston@rea-group.com>
2019-05-29 10:46:43 +1000

    feat: add logging to client connections

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,14 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 1019 });
 console.log("starting server on https://localhost:1019")
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, req) {
+  const ip = req.connection.remoteAddress;
+
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
+
+  console.log(`Client connected from ${ip}`)
 
   ws.send('something');
 });
