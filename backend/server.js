c0076f1d4b4881dac1a37e3a42c7187adca30330
Aaron Vanston <aaron.vanston@rea-group.com>
2019-05-29 11:36:12 +1000

    refactor: addFish event

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const WebSocket = require('ws')
 const chalk = require('chalk');
 
+//  SERVER CONSTS
 const PORT = 1019
 const HOST = `0.0.0.0`
+
+// EVENT CONSTS
+const addFishy = 'addFishy'
+
+// FISHY MEMORY
+let fishies = 0
 
 const wss = new WebSocket.Server({ port: PORT, host: HOST })
 
@@ -15,12 +22,17 @@ wss.on('listening', () => {
 
 wss.on('connection', (ws, req) => {
   const ip = req.connection.remoteAddress
+  console.log(`client connected from ${ip}`)
+
+  ws.send(fishies);
 
   ws.on('message', message => {
     console.log('received: %s', message)
+
+    if (message === addFishy) {
+      console.lo('Adding 1 fishy boi')
+      ++fishies
+      ws.send(fishies)
+    }
   });
-
-  console.log(`client connected from ${ip}`)
-
-  ws.send('something')
 });
