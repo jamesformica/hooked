fd583ce20215ae89237e9953117b9112db8fcbe6
Aaron Vanston <aaron.vanston@rea-group.com>
2019-05-29 16:55:34 +1000

    refactor: update time

--- common/consts.js
+++ common/consts.js
@@ -2,7 +2,7 @@ export const server = {
   PORT: '1019',
   HOST: '0.0.0.0',
   TICK_RATE: 1000,
-  FRESH_FISH: 10000, // 1000 * 10 = 10 seconds
+  FRESH_FISH: 20000, // 1000 * 10 * 2 = 20 seconds
 }
 
 export const SWIM_DURATION = 5000
