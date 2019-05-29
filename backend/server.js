d4b19e0048b78ff5087e7cde5849e575021c6d78
Aaron Vanston <aaron.vanston@rea-group.com>
2019-05-29 14:54:47 +1000

    refactor: use cooldown and send fresh fishies

--- backend/server.js
+++ backend/server.js
@@ -3,42 +3,64 @@ import chalk from 'chalk'
 
 import { server } from '../common/consts'
 import { ADD_FISHY } from '../common/events'
-const { HOST, PORT } = server
+const { HOST, PORT, TICK_RATE, FRESH_FISH } = server
 
 // FISHY MEMORY
-let fishies = 0
+let freshFishies = 0
+let stream = []
 
 const wss = new WebSocket.Server({ port: PORT, host: HOST })
 
+// Server has started
 wss.on('listening', () => {
-  const { port, address } = wss.address();
+  const { port, address } = wss.address()
   console.log(`Server running at ${chalk.bold.magenta(
     `http://${address || 'localhost'}:${port}`
   )}`)
+
+  broadcastLoop()
 })
 
 wss.on('connection', (ws, req) => {
   const ip = req.connection.remoteAddress
   console.log(`client connected from ${ip}`)
 
-  ws.send(fishies);
+  // On connection send fishy total
+  ws.send(freshFishies)
 
+  // Recieving add fishy event
   ws.on('message', message => {
-    console.log('received: %s', message)
-
     if (message === ADD_FISHY) {
-      ++fishies
-      console.log(`Adding 1 fishy boi, fishies are now at ${fishies}`)
+      const timeStamp = Date.now()
 
-      ws.send(fishies);
+      // Add to stream and increment fishy total 
+      stream.push(timeStamp)
+      ++freshFishies
 
-      // We might need to debounce this to reduce overall load
-      console.log(`Broadcasting to clients`)
-      wss.clients.forEach((client) => {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(fishies)
-        }
-      })
+      console.log(`Adding 1 fishy boi, fishies are now at ${freshFishies}`)
+
+      ws.send(freshFishies)
     }
-  });
-});
+  })
+})
+
+const broadcastLoop = () => {
+  freshFishies = calculateFreshTotal()
+  broadcastFishies()
+  setTimeout(broadcastLoop, TICK_RATE)
+}
+
+const broadcastFishies = () => {
+  console.log(`Broadcasting to clients`)
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(freshFishies)
+    }
+  })
+}
+
+const calculateFreshTotal = () => {
+  const timeNow = Date.now()
+  const totalEvents = stream.filter(event => timeNow - event < FRESH_FISH)
+  return totalEvents.length
+}
--- common/consts.js
+++ common/consts.js
@@ -1,4 +1,6 @@
 export const server = {
   PORT: '1019',
-  HOST: '0.0.0.0'
+  HOST: '0.0.0.0',
+  TICK_RATE: 1000,
+  FRESH_FISH: 10000, // 1000 * 10 = 10 seconds
 }
